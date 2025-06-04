0adb979e16fcad4b88f55a51d193cad05be9fc6f
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2025-06-04 07:03:08 +0530

    UI: Image preview close button

--- src/components/thumbnail.tsx
+++ src/components/thumbnail.tsx
@@ -2,12 +2,12 @@
 
 import {
   Dialog,
+  DialogClose,
   DialogContent,
-  DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { XIcon } from "lucide-react";
+import { Hint } from "./hint";
 
 interface ThumbnailProps {
   url: string | null | undefined;
@@ -23,16 +23,25 @@ export const Thumbnail = ({ url }: ThumbnailProps) => {
           <img
             src={url}
             alt="Message image"
-            className="rounded-md object-cover size-full"
+            className="rounded-md object-cover w-full h-full"
           />
         </div>
       </DialogTrigger>
       <DialogContent className="max-w-[800px] border-none bg-transparent p-0 shadow-none">
-        <img
-          src={url}
-          alt="Message image"
-          className="rounded-md object-cover size-full"
-        />
+        <div className="relative inline-block">
+          <Hint label="Close image">
+            <DialogClose asChild>
+              <button className="absolute -top-2.5 -right-2.5 bg-black/70 hover:bg-black text-white rounded-full size-6 flex items-center justify-center z-[4] border-2 border-white transition-colors shadow-lg">
+                <XIcon className="size-3.5" />
+              </button>
+            </DialogClose>
+          </Hint>
+          <img
+            src={url}
+            alt="Message image"
+            className="rounded-md object-contain max-w-full max-h-[80vh] w-auto h-auto"
+          />
+        </div>
       </DialogContent>
     </Dialog>
   );
